c793924257b0569f06163d77d60302385c3eaba8
ebnr321130921-pixel <ebnr321130921@gmail.com>
2026-02-22 11:25:01 +0900

    Add files via upload

---
+++ docs/keiba_bookmark/sw.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = "keiba-viewer-20260222112408";
+const CORE_ASSETS = [
+  "./",
+  "index.html",
+  "keiba_viewer.html",
+  "manifest.json",
+  "yosouka_prediction_results.html",
+  "yosouka_tipster_performance.html",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(CORE_ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    Promise.all([
+      self.clients.claim(),
+      caches.keys().then((keys) =>
+        Promise.all(keys.map((k) => (k !== CACHE_NAME ? caches.delete(k) : Promise.resolve(true))))
+      ),
+    ])
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  const req = event.request;
+  if (req.method !== "GET") return;
+
+  event.respondWith(
+    fetch(req)
+      .then((res) => {
+        const copy = res.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(req, copy)).catch(() => {});
+        return res;
+      })
+      .catch(() => caches.match(req).then((hit) => hit || caches.match("index.html")))
+  );
+});
