aced70b46b54965b7ae8c77f375d63ac4491d2de
ebnr321130921-pixel <ebnr321130921@gmail.com>
2026-03-15 15:25:37 +0900

    Update docs 20260315

--- docs/keiba_bookmark/sw.js
+++ docs/keiba_bookmark/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "keiba-viewer-20260315152502";
+const CACHE_NAME = "keiba-viewer-20260315152536";
 const CORE_ASSETS = [
   "./",
   "index.html",
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "keiba-viewer-20260315152502";
+const CACHE_NAME = "keiba-viewer-20260315152537";
 const CORE_ASSETS = [
   "./",
   "index.html",
